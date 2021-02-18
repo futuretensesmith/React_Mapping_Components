89ab6596cc162650980b45033e4cafd2a4c9165f
Kevin Smith <futuretense@gmail.com>
2021-02-18 12:27:43 -0800

    Created entry component Wrote Entry component tested in App.jsx and it works. Need to Map() emojipedia.js next.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
 function App() {
   return (
@@ -20,32 +22,11 @@ function App() {
             used in connection with doing sports, e.g. at the gym.
           </dd>
         </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+        <Entry
+          emoji={emojipedia[1].emoji}
+          name={emojipedia[1].name}
+          meaning={emojipedia[1].meaning}
+        />
       </dl>
     </div>
   );
