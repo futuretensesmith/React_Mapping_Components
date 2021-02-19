6ac3e1277d074f99531dcc6e78056f4a321229e7
Kevin Smith <futuretense@gmail.com>
2021-02-19 11:59:43 -0800

    Mapped in App.jsx updated hard coded App function the map() version.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+function createEntry(emojipedia) {
+  return (
+    <Entry
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+      aria-label={emojipedia.name}
+    />
+  );
+}
+
 function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+{
+  //****** pre-mapped hard coded tester function ******
+  /* function App() {
   return (
     <div>
       <h1>
@@ -26,10 +51,12 @@ function App() {
           emoji={emojipedia[1].emoji}
           name={emojipedia[1].name}
           meaning={emojipedia[1].meaning}
+          aria-label={emojipedia[1].name}
         />
       </dl>
     </div>
   );
+} */
 }
 
 export default App;
